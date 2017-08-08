bf483d957e6e43d9d6a7e812b22fca0c18822acc
Mike Hohne <mikehohne@gmail.com>
2017-08-07 20:41:50 -0600

    restructuring for npm use

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
+
 var request = require('request');
 
-let url = 'http://api.football-data.org/v1/competitions';
-
-var getComp = function(id){
-  request(url + "/" + id, function (error, response, body) {
-    if(error){
-      return console.error("An error occurred: " + error)
-    }
-    console.log('body:', body);
-  });
-}
-
-var getTeams = function(id) {
+exports.getTeams = function(id) {
   var url = "http://api.football-data.org/v1/teams"
   request(url + "/" + id, function (error, response, body) {
     if(error){
       return console.error("An error occurred: " + error)
     }
     console.log('body:', body);
-  })
-}
+    })
+  },
 
-var getAllComps = function(id,info,includeTeams){
+exports.getAllComps = function(id,info,includeTeams){
   if(id){
     url += "/" + id + "/" + info;
   }
